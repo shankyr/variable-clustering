6d46db7d8e3081e40bb52e65ca38ece530925478
shankyr <rshashankr@gmail.com>
2018-10-25 19:46:58 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='decomposition',
+      version='0.2.5',
+      description='Variable clustering',
+      url='https://github.com/shankyr/variable-clustering',
+      author='Jingmin1987',
+      author_email='Jingmin1987@gmail.com',
+      license='Apache License, Version 2.0',
+      packages=['decomposition'],
+      zip_safe=False)
